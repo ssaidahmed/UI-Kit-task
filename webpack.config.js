e9702c5fdd402d21e9658b82c576bc8412ca2f22
ssaidahmed <ssaidahmed@gmail.com>
2019-08-07 16:50:54 +0300

    Fix error read file and add pug loader, add plugins for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const path = require('path');
+const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist'),
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist'),
   assets: 'assets/'
 }
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+
+const PAGES = fs.readdirSync(PAGES_DIR, 'utf8').filter(filename => filename.endsWith('.pug'));
+
 module.exports = {
   entry: {
     app: `${PATHS.src}/index.js`
   },
   output:{
     path: PATHS.dist,
-    filename: `${PATHS.src}js/[name].[hash].js`,
+    filename: `${PATHS.assets}js/[name].[hash].js`,
     publicPath: '/'
     
 
@@ -19,6 +28,10 @@ module.exports = {
   module: {
 
     rules: [
+      {
+        test:/\.pug$/,
+        loader: 'pug-loader',
+      },
       {
         test:/\.scss$/,
         use: [
@@ -33,6 +46,21 @@ module.exports = {
       }
     ]
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[hash].css`,
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+      { from: `${PATHS.src}/static`, to: '' },
+    ]),
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/, '.html')}`
+    }))
+  ],
   devServer: {
     overlay: true
   }
